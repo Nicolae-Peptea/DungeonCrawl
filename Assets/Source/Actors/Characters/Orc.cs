5c08841da2006df678f830a58c6d909573a7f7b9
Nicolae <Nicolae@NICU>
2021-09-08 15:37:15 +0300

    Revert "change heatlh and attack for ghost and Orc"
    This reverts commit b988c1eadee0dcbb6f46467aa181b07f87cb712c.

--- Assets/Source/Actors/Characters/Orc.cs
+++ Assets/Source/Actors/Characters/Orc.cs
@@ -4,9 +4,9 @@ namespace DungeonCrawl.Actors.Characters
 {
     public class Orc : Character
     {
-        public override int Health { get; protected set; } = 20;
+        public override int Health { get; protected set; } = 100;
 
-        public override int Attack { get; protected set; } = 7;
+        public override int Attack { get; protected set; } = 5;
 
         public override bool OnCollision(Actor anotherActor)
         {
